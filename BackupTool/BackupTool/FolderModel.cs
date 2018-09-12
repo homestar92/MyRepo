3f90cf717f52a2257f234b534f1ee6142400405a
homestar92 <homestar92@gmail.com>
2018-09-12 19:41:25 -0400

    Initial Commit

---
+++ BackupTool/BackupTool/FolderModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupTool
+{
+    public class FolderModel : INotifyPropertyChanged
+    {
+        private string _folder { get; set;  }
+
+        public string Folder
+        {
+            get { return _folder; }
+            set
+            {
+                _folder = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Folder"));
+                }
+            }
+        }
+
+        public FolderModel(string folder)
+        {
+            this.Folder = folder;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
