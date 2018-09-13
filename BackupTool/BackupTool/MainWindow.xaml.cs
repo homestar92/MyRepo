9abe03c512fe9bc6db14322d128f27caa689cf15
homestar92 <homestar92@gmail.com>
2018-09-12 20:21:44 -0400

    Incremental change

--- BackupTool/BackupTool/MainWindow.xaml.cs
+++ BackupTool/BackupTool/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BackupTool
 {
@@ -20,13 +12,64 @@ namespace BackupTool
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<FolderModel> SelectedFolders { get; set;  }
+        public ObservableCollection<FolderModel> SelectedFolders { get; set; }
+
+        private ICommand m_deleteCommand;
+        public ICommand DeleteCommand
+        {
+            get
+            {
+                if (m_deleteCommand == null)
+                {
+                    m_deleteCommand = new RelayCommand(param => Delete((FolderModel)param));
+                }
+                return m_deleteCommand;
+            }
+        }
+
+        private void Delete(FolderModel result)
+        {
+            SelectedFolders.Remove(result);
+        }
 
         public MainWindow()
         {
-            SelectedFolders = new List<FolderModel>();
+            SelectedFolders = new ObservableCollection<FolderModel>();
             InitializeComponent();
-            SelectedFolders.Add(new FolderModel("Hello!"));
+            dataGrid.ItemsSource = SelectedFolders;
+        }
+
+        public static string NormalizePath(string path)
+        {
+            return System.IO.Path.GetFullPath(new Uri(path).LocalPath)
+                .TrimEnd(System.IO.Path.DirectorySeparatorChar, System.IO.Path.AltDirectorySeparatorChar)
+                .ToUpperInvariant();
+        }
+
+        private void BtnAddFolder_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    string path = dialog.SelectedPath;
+                    if (Directory.Exists(path) && !SelectedFolders.Any(x => NormalizePath(x.Folder) == NormalizePath(path)))
+                    {
+                        SelectedFolders.Add(new FolderModel(path));
+                    }
+                }
+            }
+        }
+
+        private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
